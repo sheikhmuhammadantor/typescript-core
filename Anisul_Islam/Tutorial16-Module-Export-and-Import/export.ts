c1981b11f93de68c15179ee3a1c7fe08c400f5f9
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-03-03 19:56:12 +0600

    Add Module Export and Import.

---
+++ Anisul_Islam/Tutorial16-Module-Export-and-Import/export.ts
@@ -0,0 +1,2 @@
+export let userName = "Antro";
+export let userAge = 21;
---
+++ Anisul_Islam/Tutorial16-Module-Export-and-Import/inport.ts
@@ -0,0 +1,3 @@
+import * as User from "./export";
+
+console.log(`User Name : ${User.userName} and User Age : ${User.userAge}`);
